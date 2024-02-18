5c6d4b2705eb9fb5faa2e31b38a818a1c20cef6c
Jetsada <jetsada.hpun@gmail.com>
2024-02-18 09:19:09 +0700

    .

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+      <div className="grid grid-cols-[40%_60%]">
+        <div>Left</div>
+        <div>Right</div>
+
+      </div>
+  );
+}
+
+export default App;
